5eae1bb2bba7d6b86e760b7a18060bb6817f192d
KelechiOdom10 <kelechiodom10@gmail.com>
2020-05-25 11:42:48 +0100

    Added classNames and changed the structure in order to style it better

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { WiStrongWind, WiHumidity, WiSunrise, WiSunset  } from "react-icons/wi"
 
 function WeatherCard(props) {
     return(
-        <div>
-            <h1>Name: {`${props.name}, ${props.country}`}</h1>
-            <p>Date: {props.date}</p>
-            <p>Time: {props.time}</p>
-            <h2>Main Temp: {props.temp}&deg;C</h2>
-            <p>Feels like {props.feelsLike}&deg;C</p>
-            <p>Humidity: {props.humidity}%</p>
-            <p><span><img src={props.icon} alt='Weather icon' /></span>Weather Description: {props.description}</p>
-            <p>Wind: {props.wind}m/s</p>
-            <p>Sunrise: {props.sunrise}</p>
-            <p>Sunset: {props.sunset}</p>
+        <div className='weatherCard'>
+            <h1 className='mainHeader'>{`${props.name}, ${props.country}`}</h1>            
+            <p>Date: {props.date} <span> | {props.time}</span></p>
+            <div className='weather-wrapper'>
+                <div className="weather-box">
+                    <h2 className="temp">{props.temp}&deg;C
+                    <span className="feelsLike"><p>Feels like {props.feelsLike}&deg;C</p></span>
+                    </h2> 
+                    <div className='weather-info'><img src={props.icon} alt='Weather icon' style={{width: '10vh'}} /><span><p>{props.description}</p></span> </div>   
+                </div>
+                <div className="weather-data">
+                                
+                    <p className='weather-info'><span><WiHumidity /></span> {props.humidity}%</p>
+                    <p className='weather-info'><span><WiStrongWind /></span>{props.wind}m/s</p>
+                    <p className='weather-info'><span><WiSunrise /></span> Sunrise: {props.sunrise}</p>
+                    <p className='weather-info'><span><WiSunset/></span> Sunset: {props.sunset}</p>       
+                </div>
+            </div>
+                    
+            
+            
       </div>
     )
 }
